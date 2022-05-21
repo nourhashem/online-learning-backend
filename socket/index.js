fb6c58d5f121ab9862863871d6227545765301cd
Nour Hashem <noor.hashem.1998@gmail.com>
2022-05-21 12:41:09 +0300

    add events file

--- socket/index.js
+++ socket/index.js
@@ -1,18 +1,24 @@
 const users = require('./users');
 const userController = require('../controllers/user');
 const { USER_ROLES } = require('../utils/constants');
+const events = require('./events');
 
 const init = () => {
-  io.on('connection', (socket) => {
-    io.emit('message', `HELLO ${socket.id}`);
-    io.to('d3b38471-2610-4ae5-a32b-14fd020435d9').emit(
-      'message',
-      'HELLO CLASSROOM'
-    );
+  io.on(events.CONNECTION, (socket) => {
     const userUuid = socket.handshake.query.userUuid;
-    users.add(userUuid, socket.id);
-    joinRooms(socket, userUuid);
-    socket.on('disconnect', () => {
+    if (userUuid) {
+      joinRooms(socket, userUuid);
+      users.add(userUuid, socket.id);
+    } else {
+      socket.disconnect();
+    }
+
+    socket.on(events.SEND_MESSAGE, (data) => {
+      console.log('received SEND_MESSAGE', data);
+      io.to(data.classroomUuid).emit(events.MESSAGE, data);
+    });
+
+    socket.on(events.DISCONNECT, () => {
       console.log('disconnect', socket.id);
       users.remove(userUuid, socket.id);
     });
@@ -21,17 +27,19 @@ const init = () => {
 
 const joinRooms = async (socket, userUuid) => {
   const user = await userController.getByUuid(userUuid);
-  let classrooms = [];
-  if (user.role === USER_ROLES.STUDENT) {
-    classrooms = await user.getStudentClassrooms();
-  }
-  if (user.role === USER_ROLES.INSTRUCTOR) {
-    classrooms = await user.getInstructorClassrooms();
-  }
-  for (let i = 0; i < classrooms.length; i++) {
-    const classroom = classrooms[i];
-    console.log(socket.id, 'joining', classroom.uuid);
-    socket.join(classroom.uuid);
+  if (user) {
+    let classrooms = [];
+    if (user.role === USER_ROLES.STUDENT) {
+      classrooms = await user.getStudentClassrooms();
+    }
+    if (user.role === USER_ROLES.INSTRUCTOR) {
+      classrooms = await user.getInstructorClassrooms();
+    }
+    for (let i = 0; i < classrooms.length; i++) {
+      const classroom = classrooms[i];
+      console.log(socket.id, 'joining', classroom.uuid);
+      socket.join(classroom.uuid);
+    }
   }
 };
 
