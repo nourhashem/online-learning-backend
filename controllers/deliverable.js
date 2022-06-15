3f037f0c9e45965eca774b113ca20d1c9fecac63
Nour Hashem <noor.hashem.1998@gmail.com>
2022-06-15 23:46:29 +0300

    fix autocorrect

--- controllers/deliverable.js
+++ controllers/deliverable.js
@@ -1,100 +1,99 @@
 const db = require('../models');
 
 const add = (deliverableObj) =>
-	new Promise((resolve, reject) => {
-		db.Deliverable.create(deliverableObj, {
-			include: [
-				{
-					association: db.Deliverable.classroom,
-				},
-				db.Question.deliverable,
-			],
-		})
-			.then((deliverables) => {
-				resolve(deliverables);
-			})
-			.catch((error) => {
-				if (error && error.errors) {
-					const errorMessage = error.errors
-						.map((errorObj) => errorObj.message)
-						.join();
-					reject(errorMessage);
-				} else {
-					reject('Deliverable creation failed');
-				}
-			});
-	});
+  new Promise((resolve, reject) => {
+    db.Deliverable.create(deliverableObj, {
+      include: [
+        {
+          association: db.Deliverable.classroom,
+        },
+        db.Question.deliverable,
+      ],
+    })
+      .then((deliverables) => {
+        resolve(deliverables);
+      })
+      .catch((error) => {
+        if (error && error.errors) {
+          const errorMessage = error.errors
+            .map((errorObj) => errorObj.message)
+            .join();
+          reject(errorMessage);
+        } else {
+          reject('Deliverable creation failed');
+        }
+      });
+  });
 
 const getAll = (classroomUuid) =>
-	new Promise((resolve, reject) => {
-		db.Deliverable.findAll({
-			where: {
-				classroomUuid: classroomUuid,
-			},
-			include: [
-				{
-					model: db.Question,
-					as: 'questions',
-				},
-			],
-		})
-			.then((deliverables) => resolve(deliverables))
-			.catch((error) => reject(error));
-	});
+  new Promise((resolve, reject) => {
+    db.Deliverable.findAll({
+      where: {
+        classroomUuid: classroomUuid,
+      },
+      include: [
+        {
+          model: db.Question,
+          as: 'questions',
+        },
+      ],
+    })
+      .then((deliverables) => resolve(deliverables))
+      .catch((error) => reject(error));
+  });
 
 const getByUuid = (deliverableUuid) =>
-	new Promise((resolve, reject) => {
-		db.Deliverable.findByPk(deliverableUuid, {
-			include: [
-				{
-					model: db.Question,
-					as: 'questions',
-				},
-			],
-		})
-			.then((deliverable) => resolve(deliverable))
-			.catch((error) => reject(error));
-	});
+  new Promise((resolve, reject) => {
+    db.Deliverable.findByPk(deliverableUuid, {
+      include: [
+        {
+          model: db.Question,
+          as: 'questions',
+        },
+      ],
+    })
+      .then((deliverable) => resolve(deliverable))
+      .catch((error) => reject(error));
+  });
 
 const checkAnswer = (studentAnswer, answers) => {
-	const myAnswer = answers.find((a) => a.uuid === studentAnswer.uuid);
-	if (myAnswer) {
-		const answer = { ...myAnswer, answer: JSON.parse(myAnswer.answer) };
-		if (answer.type === 'multiple') {
-			let correct = true;
-			for (let j = 0; j < choices.length; j++) {
-				if (answer.answer[j] !== studentAnswer.answer[j]) {
-					correct = false;
-					return false;
-				}
-			}
-			if (correct) return true;
-		} else if (answer.answer === studentAnswer.answer) {
-			return true;
-		}
-		return false;
-	} else {
-		return false;
-	}
+  if (!studentAnswer) return false;
+  const answer = answers.find((a) => a.uuid === studentAnswer.uuid);
+  if (answer) {
+    if (answer.type === 'multiple') {
+      let correct = true;
+      for (let j = 0; j < answer.choices.length; j++) {
+        if (answer.answer[j] !== studentAnswer.answer[j]) {
+          correct = false;
+          return false;
+        }
+      }
+      if (correct) return true;
+    } else if (answer.answer === studentAnswer.answer) {
+      return true;
+    }
+    return false;
+  } else {
+    return false;
+  }
 };
 
 const calculateGrade = (studentAnswers, answers) => {
-	console.log('calculateGrade');
-	const numTotal = answers.length;
-	let numCorrect = 0;
-	for (let i = 0; i < studentAnswers.length; i++) {
-		const studentAnswer = studentAnswers[i];
-		if (studentAnswer.correct) numCorrect += 1;
-	}
-	const grade = (numCorrect / numTotal) * 100;
-	console.log({ numTotal, numCorrect, grade });
-	return grade;
+  const numTotal = answers.length;
+  let numCorrect = 0;
+  for (let i = 0; i < studentAnswers.length; i++) {
+    const studentAnswer = studentAnswers[i];
+    if (studentAnswer.correct) numCorrect += 1;
+  }
+  const grade = (numCorrect / numTotal) * 100;
+  console.log({ numTotal, numCorrect, grade });
+  return grade;
 };
 
 module.exports = {
-	add,
-	getAll,
-	getByUuid,
-	checkAnswer,
-	calculateGrade,
+  add,
+  getAll,
+  getByUuid,
+  checkAnswer,
+  calculateGrade,
 };
--- routes/attempts.js
+++ routes/attempts.js
@@ -6,76 +6,77 @@ const { authToken } = require('../utils/jwt');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/', authToken, async (req, res, next) => {
-	const deliverableUuid = req.query.deliverableUuid;
-	const attempts = await attemptController.getAll(deliverableUuid);
-	const attemptsJSON = [];
-	for (let i = 0; i < attempts.length; i++) {
-		const attemptJSON = attempts[i].dataValues;
-		attemptsJSON.push(attemptJSON);
-	}
-	const response = attemptsJSON.map((attempt) => ({
-		...attempt,
-		questions: attempt.questions.map((question) => ({
-			...question.dataValues,
-			answer: JSON.parse(question.dataValues.answer),
-			choices: JSON.parse(question.dataValues.choices),
-		})),
-	}));
-	res.send({ attempts: response });
+  const deliverableUuid = req.query.deliverableUuid;
+  const attempts = await attemptController.getAll(deliverableUuid);
+  const attemptsJSON = [];
+  for (let i = 0; i < attempts.length; i++) {
+    const attemptJSON = attempts[i].dataValues;
+    attemptsJSON.push(attemptJSON);
+  }
+  const response = attemptsJSON.map((attempt) => ({
+    ...attempt,
+    questions: attempt.questions.map((question) => ({
+      ...question.dataValues,
+      answer: JSON.parse(question.dataValues.answer),
+      choices: JSON.parse(question.dataValues.choices),
+    })),
+  }));
+  res.send({ attempts: response });
 });
 
 router.get('/:attemptUuid', authToken, async (req, res, next) => {
-	const attemptUuid = req.params.attemptUuid;
-	const attempt = await attemptController.getByUuid(attemptUuid);
-	const attemptData = {
-		...attempt.dataValues,
-		questions: attemptData.questions.map((q) => {
-			const { answer, choices, ...withoutAnswer } = q.dataValues;
-			return {
-				...withoutAnswer,
-				answer: JSON.parse(answer),
-				choices: JSON.parse(choices),
-			};
-		}),
-	};
-	res.send({ attempt: attemptData });
+  const attemptUuid = req.params.attemptUuid;
+  const attempt = await attemptController.getByUuid(attemptUuid);
+  const attemptData = {
+    ...attempt.dataValues,
+    questions: attemptData.questions.map((q) => {
+      const { answer, choices, ...withoutAnswer } = q.dataValues;
+      return {
+        ...withoutAnswer,
+        answer: JSON.parse(answer),
+        choices: JSON.parse(choices),
+      };
+    }),
+  };
+  res.send({ attempt: attemptData });
 });
 
 router.post('/', authToken, async (req, res, next) => {
-	try {
-		console.log('my_request');
-		const deliverableUuid = req.body.deliverableUuid;
-		const deliverable = await deliverableController.getByUuid(
-			deliverableUuid
-		);
-		const questions = deliverable.dataValues.questions.map(
-			(q) => q.dataValues
-		);
-		console.log('my_questions', questions);
-		const attemptUuid = uuidv4();
-		const answers = req.body.data.map((a) => ({
-			...a,
-			uuid: uuidv4(),
-			attemptUuid,
-			deliverableUuid: null,
-			choices: JSON.stringify(a.choices),
-			answer: JSON.stringify(a.answer),
-			correct: deliverableController.checkAnswer(a, questions),
-		}));
-		console.log('my_answers', answers);
-		const attemptObj = {
-			uuid: attemptUuid,
-			deliverableUuid,
-			studentUuid: req.userUuid,
-			questions: answers,
-			grade: deliverableController.calculateGrade(answers, questions),
-		};
-		console.log('my_attempt', attemptObj);
-		await attemptController.add(attemptObj);
-		res.send({ message: 'success' });
-	} catch (error) {
-		res.send({ error });
-	}
+  try {
+    console.log('my_request');
+    const deliverableUuid = req.body.deliverableUuid;
+    const deliverable = await deliverableController.getByUuid(deliverableUuid);
+    const questions = deliverable.dataValues.questions.map((q) => ({
+      ...q.dataValues,
+      choices: JSON.parse(q.dataValues.choices),
+      answer: JSON.parse(q.dataValues.answer),
+    }));
+    console.log('my_questions', questions);
+    console.log('my_data', req.body.data);
+    const attemptUuid = uuidv4();
+    const answers = req.body.data.filter(Boolean).map((a) => ({
+      ...a,
+      uuid: uuidv4(),
+      attemptUuid,
+      deliverableUuid: null,
+      choices: JSON.stringify(a.choices),
+      answer: JSON.stringify(a.answer),
+      correct: deliverableController.checkAnswer(a, questions),
+    }));
+    console.log('my_answers', answers);
+    const attemptObj = {
+      uuid: attemptUuid,
+      deliverableUuid,
+      studentUuid: req.userUuid,
+      questions: answers,
+      grade: deliverableController.calculateGrade(answers, questions),
+    };
+    console.log('my_attempt', attemptObj);
+    await attemptController.add(attemptObj);
+    res.send({ message: 'success' });
+  } catch (error) {
+    res.send({ error });
+  }
 });
 
 module.exports = router;
